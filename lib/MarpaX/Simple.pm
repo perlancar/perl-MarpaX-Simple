e385294302f1d16edcaacac80bab6140f3347933
Steven Haryanto (on PC, Bandung) <stevenharyanto@gmail.com>
2014-05-24 14:58:32 +0700

    Add arguments slif_grammar, grammar_class_file, grammar_module_file, grammar_file

--- lib/MarpaX/Simple.pm
+++ lib/MarpaX/Simple.pm
@@ -19,13 +19,57 @@ our %SPEC;
 $SPEC{gen_parser} = {
     v => 1.1,
     summary => 'Generate Marpa-based parser',
+    description => <<'_',
+
+One of `slif_grammar`, `grammar_class_file`, `grammar_module_file`,
+`grammar_file`, `grammar` must be specified for the input grammar. The arguments
+will be checked in that order. The simplest would be to use grammar text inlined
+in your source code (`grammar`), but for larger grammar it might be more
+convenient to put it in a separate file (the `grammar_*file` arguments) so you
+can check line numbers from error messages more easily. The `slif_grammar` has
+the lowest overhead since you specify SLIF grammar that are already
+instantiated. See also the related Dist::Zilla plugin
+`Dist::Zilla::Plugin::InsertMarpaSLIFGrammar`.
+
+
+_
     args => {
-        grammar => {
-            summary => '',
-            schema  => 'str*',
-            req => 1,
-            pos => 0,
+        slif_grammar => {
+            summary => 'SLIF input grammar',
+            schema  => ['obj*', isa=>'Marpa::R2::Scanless::G'],
+            description => <<'_',
+
+This input-grammar argument is checked first because it is the most ready-to-use
+form and has the least overhead. The other input-grammar arguments require
+creating the `Marpa::R2::Scanless::G` object first from the Marpa BNF DSL
+grammar.
+
+_
+            tags    => ['category:input-grammar'],
         },
+        grammar_class_file => {
+            summary => 'Marpa BNF input grammar file, '.
+                'resolved using File::ShareDir\'s class_file()',
+            schema  => 'str*',
+            tags    => ['category:input-grammar'],
+        },
+        grammar_module_file => {
+            summary => 'Marpa BNF input grammar file, '.
+                'resolved using File::ShareDir\'s module_file()',
+            schema  => 'str*',
+            tags    => ['category:input-grammar'],
+        },
+        grammar_file => {
+            summary => 'Marpa BNF input grammar file',
+            schema  => 'str*',
+            tags    => ['category:input-grammar'],
+        },
+        grammar => {
+            summary => 'Marpa BNF input grammar text',
+            schema  => 'str*',
+            tags    => ['category:input-grammar'],
+        },
+
         actions => {
             summary => 'Supply code for actions specified in the grammar',
             schema  => ['hash*', each_value => 'code*'],
@@ -53,20 +97,44 @@ sub gen_parser {
 
     my %args = @_;
 
-    $log->tracef("Precomputing grammar ...");
-    my $grammar = Marpa::R2::Scanless::G->new({source => \$args{grammar} });
+    my $slif;
+    {
+        my $bnf;
+        if ($args{slif_grammar}) {
+            $slif = $args{slif_grammar};
+            last;
+        } elsif ($args{grammar_class_file}) {
+            require File::ShareDir;
+            require File::Slurp::Tiny;
+            $bnf = File::Slurp::Tiny::read_file(
+                File::ShareDir::class_file(__PACKAGE__,
+                                           $args{grammar_class_file}));
+        } elsif ($args{grammar_module_file}) {
+            require File::ShareDir;
+            require File::Slurp::Tiny;
+            $bnf = File::Slurp::Tiny::read_file(
+                File::ShareDir::module_file(__PACKAGE__,
+                                            $args{grammar_module_file}));
+        } elsif ($args{grammar_file}) {
+            require File::Slurp::Tiny;
+            $bnf = File::Slurp::Tiny::read_file($args{grammar_file});
+        } elsif ($args{grammar}) {
+            $bnf = $args{grammar};
+        }
+        $slif = Marpa::R2::Scanless::G->new({source => \$bnf});
+    }
+
     my $pkg = __PACKAGE__ . '::gen' . substr(UUID::Random::generate(), 0, 8);
     my $acts = $args{actions};
     for (keys %$acts) {
         *{"$pkg\::$_"} = $acts->{$_};
     }
 
-    $log->tracef("Generating parser ...");
     my $parser = sub {
         my $input = shift;
 
         my $rec_args = {
-            grammar => $grammar,
+            grammar => $slif,
             semantics_package => $pkg,
             trace_terminals => $args{trace_terminals} ? 1:0,
             trace_values    => $args{trace_values}    ? 1:0,
@@ -114,7 +182,7 @@ which is a shortcut for roughly this:
 
  no strict 'refs';
  use Marpa::R2;
- my $grammar = Marpa::R2::Scanless::G->new({source => \$args{grammar}});
+ my $slif = Marpa::R2::Scanless::G->new({source => \$args{grammar}});
  my $pkg = "MarpaX::Simple::gen" . some_random_value();
  my $actions = $args{actions};
  for (keys %$actions) {
@@ -123,7 +191,7 @@ which is a shortcut for roughly this:
  my $parser = sub {
      my $input = shift;
      my $recce = Marpa::R2::Scanless::R->new({
-         grammar => $grammar,
+         grammar => $slif,
          semantics_package => $pkg,
      });
  };
